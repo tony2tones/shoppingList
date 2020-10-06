e19dd49a14a84108f9bb1eb2a9e775208e8d4614
Tony <anthonymarques@outlook.com>
2020-10-06 09:50:41 +0200

    WORK IN PROGRESS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { ShoppingListService } from './services/shopping-list.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   constructor(private shoppingList: ShoppingListService) { }
   title = 'shoppingList';
   response: any;
@@ -19,7 +19,7 @@ export class AppComponent implements OnInit{
     // this.shoppingList.getBasicGroceries().subscribe(items => {
     // this.items = items;
     console.log(this.items);
-  // });
+    // });
   }
 
   getShoppingList() {
@@ -35,12 +35,39 @@ export class AppComponent implements OnInit{
   }
 
   putToArray(array) {
-    this.list = array.basic; 
+    this.list = array.basic;
     // array['Pesto Chicken'];
     this.show = true;
     // console.log('The list ', this.list);
     console.log('The list ', array);
     console.log('The list ', this.list);
+    let newArray = [];
+    newArray = this.list;
+
+    // console.log(newArray[0]);
+    // console.log(newArray[2]);
+    // console.log(newArray[3]);
+    // console.log(newArray[4]);
+
+    // let returnedArray = [];
+    // var test = ['a', 'b', 'c', 'd'];
+
+    // var output = test.map(setID);
+    // console.log(output);
+    let testArr:any = [];
+    for ( var i=0, n=newArray.length; i<n; i++){
+      for (var j =0; newArray[i].length; j++) {
+        testArr = newArray[i][j] = `test`;
+      }
+      newArray[i] = `id:${i}` + 'name:' + newArray[i]
+     }
+
+    console.log('testArr ', testArr);
+  }
+
+  setID(item, index) {
+    var key = "id: " + item;
+    return key;
   }
 
 }
